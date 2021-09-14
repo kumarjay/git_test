1558b0d47c017bca87279b862715784e56f0a83f
kumarjay <kumarjay2107@gmail.com>
2021-09-15 00:14:18 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# …or create a new repository on the command line
+# echo "# git_test" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/kumarjay/git_test.git
+# git push -u origin main
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/kumarjay/git_test.git
+# git branch -M main
+# git push -u origin main
